c1d512e074fa80fb5497fe05a0af7b41f9dc63d4
abhishekpathania <abhishek@iomete.com>
2025-05-07 13:58:00 +0530

    Add test cases to validate on schema changes for append and fix issue with this strategy where column is deleted from model in incremental run

--- dbt/include/iomete/macros/materializations/incremental/strategies.sql
+++ dbt/include/iomete/macros/materializations/incremental/strategies.sql
@@ -11,17 +11,27 @@
 
 {% macro get_insert_into_sql(source_relation, target_relation) %}
 
-    {%- set dest_columns = adapter.get_columns_in_relation(target_relation) -%}
-    {%- set dest_cols_csv = dest_columns | map(attribute='quoted') | join(', ') -%}
-    insert into table {{ target_relation }}
-    select {{dest_cols_csv}} from {{ source_relation.include(database=false, schema=false) }}
+    {%- set dest_columns = adapter.get_columns_in_relation(target_relation) | map(attribute='quoted') | list -%}
+    {%- set source_columns = adapter.get_columns_in_relation(source_relation) | map(attribute='quoted') | list -%}
+
+    insert into table {{ target_relation }} ({{ dest_columns | join(', ') }})
+    select
+        {%- for col in dest_columns %}
+            {%- if col in source_columns -%}
+                {{ col }}
+            {%- else -%}
+                NULL AS {{ col }}
+            {%- endif -%}
+            {%- if not loop.last %}, {% endif -%}
+        {%- endfor %}
+    from {{ source_relation.include(database=false, schema=false) }}
 
 {% endmacro %}
 
 
 {% macro iomete__get_merge_sql(target, source, unique_key, dest_columns, predicates=none) %}
   {%- set predicates = [] -%}
-  {%- set source_columns = adapter.get_columns_in_relation(source) -%}
+  {%- set source_columns = adapter.get_columns_in_relation(source) | map(attribute='quoted') | list -%}
   {%- set dest_columns = adapter.get_columns_in_relation(target) -%}
   {%- set merge_update_columns = config.get('merge_update_columns') -%}
   {%- set merge_exclude_columns = config.get('merge_exclude_columns') -%}
@@ -29,14 +39,14 @@
 
   {%- set update_columns = [] -%}
   {%- for column_name in all_update_columns -%}
-    {% if column_name in source_columns | map(attribute='quoted') | list %}
+    {% if column_name in source_columns %}
       {% do update_columns.append(column_name) %}
     {% endif %}
   {%- endfor -%}
 
   {%- set insert_columns = [] -%}
   {%- for col in dest_columns -%}
-    {% if col.quoted in source_columns | map(attribute='quoted') | list %}
+    {% if col.quoted in source_columns %}
       {% do insert_columns.append(col.quoted) %}
     {% endif %}
   {%- endfor -%}
@@ -72,11 +82,7 @@
         {%- endfor %}
         {%- else %} * {% endif %}
 
-      when not matched then insert (
-        {%- for column_name in insert_columns %}
-          {{ column_name }}{% if not loop.last %}, {% endif %}
-        {%- endfor %}
-      ) values (
+      when not matched then insert ({{ insert_columns | join(', ') }}) values (
         {%- for column_name in insert_columns %}
           DBT_INTERNAL_SOURCE.{{ column_name }}{% if not loop.last %}, {% endif %}
         {%- endfor %}
--- tests/functional/adapter/incremental/test_incremental_on_schema_change.py
+++ tests/functional/adapter/incremental/test_incremental_on_schema_change.py
@@ -1,7 +1,25 @@
+import pytest
 from dbt.tests.adapter.incremental.test_incremental_on_schema_change import (
     BaseIncrementalOnSchemaChange
 )
 
+class TestIncrementalMergeOnSchemaChange(BaseIncrementalOnSchemaChange):
+    @pytest.fixture(scope="class")
+    def project_config_update(self):
+        return {
+            "models": {
+                "+incremental_strategy": "merge",
+                "+file_format": "iceberg",
+            }
+        }
 
-class TestIncrementalOnSchemaChange(BaseIncrementalOnSchemaChange):
-    pass
+
+class TestIncrementalAppendOnSchemaChange(BaseIncrementalOnSchemaChange):
+    @pytest.fixture(scope="class")
+    def project_config_update(self):
+        return {
+            "models": {
+                "+incremental_strategy": "append",
+                "+file_format": "iceberg",
+            }
+        }
