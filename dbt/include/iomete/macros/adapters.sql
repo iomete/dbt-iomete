2de3b0779ea9cf28f588a43b78b3b105836bb6cd
abhishekpathania <abhishek@iomete.com>
2025-05-08 18:43:58 +0530

    Add table properties support

--- dbt/include/iomete/macros/adapters.sql
+++ dbt/include/iomete/macros/adapters.sql
@@ -107,6 +107,7 @@
         {{ clustered_cols(label="clustered by") }}
         {{ location_clause() }}
         {{ comment_clause() }}
+        {{ tblproperties_clause() }}
         as
           {{ compiled_code }}
       {%- endif %}
@@ -126,6 +127,7 @@
 {% macro iomete__create_view_as(relation, sql) -%}
   create or replace view {{ relation }}
   {{ comment_clause() }}
+  {{ tblproperties_clause() }}
   as
     {{ sql }}
 {% endmacro %}
