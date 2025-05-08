295d4505bd7462de4bd0b3c126312d02a36ed876
abhishekpathania <abhishek@iomete.com>
2025-05-08 14:32:48 +0530

    Add incremental predicate support

---
+++ tests/functional/adapter/incremental/test_incremental_predicates.py
@@ -0,0 +1,28 @@
+import pytest
+from dbt.tests.adapter.incremental.test_incremental_predicates import BaseIncrementalPredicates
+
+
+@pytest.mark.focus
+class TestIncrementalPredicatesDeleteInsert(BaseIncrementalPredicates):
+    @pytest.fixture(scope="class")
+    def project_config_update(self):
+        return {
+            "models": {
+                "+incremental_predicates": ["dbt_internal_dest.id != 2"],
+                "+incremental_strategy": "merge",
+                "+file_format": "iceberg"
+            }
+        }
+
+
+@pytest.mark.focus
+class TestPredicatesDeleteInsert(BaseIncrementalPredicates):
+    @pytest.fixture(scope="class")
+    def project_config_update(self):
+        return {
+            "models": {
+                "+predicates": ["dbt_internal_dest.id != 2"],
+                "+incremental_strategy": "merge",
+                "+file_format": "iceberg"
+            }
+        }
